34d71570b8d98116cffd2a6bc1498fc02b5030da
Marcos DCP <zs20018147@studiantes.uv.mx>
2023-02-28 11:23:50 -0600

    hola

--- practica02/Services/Operaciones.cs
+++ practica02/Services/Operaciones.cs
@@ -1,19 +1,34 @@
 using System;
 using WSDL.mensajes;
 
-namespace WSDL.operaciones
-{
-    public class Operaciones : Mensajes
-    {
-        public string Saludar(string nombre)
-        {
-            string msj = "Hola" + nombre;
-            return msj;
+namespace WSDL.operaciones{
+    public class Operaciones : Mensajes{
+        private string[] mensajes = new string[2];
+ 
+        public string Saludar(string nombre){
+            Boolean estaLleno = true;
+            for(int x = 0; x < mensajes.Length; x++){
+                if(string.IsNullOrEmpty(mensajes[x])){
+                    mensajes[x]=nombre;
+                    estaLleno = false;
+                    break;
+                }
+            }
+
+            if (estaLleno == true){
+                return "Memoria llena..";
+            }
+            return "Hola "+nombre;
         }
-        public string Mostrar(int id)
-        {
-            return "x";
+        public string Mostrar(int id){
+            if(id >= mensajes.Length || id < 0){
+                return "No existe este id";
+            } 
+
+            if(!string.IsNullOrEmpty(mensajes[id])){
+                return "Hola y bienvenido " + mensajes[id];
+            }
+            return "No se encontro el mensaje con el id";
         }
     }
-
 }
