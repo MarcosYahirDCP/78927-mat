e97f64e5e613ff369cd6c8b18debc48d4bf8a5d3
Marcos DCP <zs20018147@studiantes.uv.mx>
2023-03-31 11:27:20 -0600

    juhg_

--- practica04/src/main/java/mx/uv/practica04/EndPoint.java
+++ practica04/src/main/java/mx/uv/practica04/EndPoint.java
@@ -1,37 +1,45 @@
 package mx.uv.practica04;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
-import org.springframework.xml.xpath.NodeMapper;
 
+import https.t4is_uv_mx.saludos.BorrarRequest;
+import https.t4is_uv_mx.saludos.BorrarResponse;
 import https.t4is_uv_mx.saludos.ModificarRequest;
 import https.t4is_uv_mx.saludos.ModificarResponse;
 import https.t4is_uv_mx.saludos.PedirResponse;
+import https.t4is_uv_mx.saludos.PedirUnoRequest;
+import https.t4is_uv_mx.saludos.PedirUnoResponse;
 import https.t4is_uv_mx.saludos.SaludarRequest;
 import https.t4is_uv_mx.saludos.SaludarResponse;
 
 @Endpoint
 public class EndPoint {
-    String [] nombres = new String[100];
-    int i=0;
+    ArrayList<String> nombres = new ArrayList<String>();
+    Iterable<Saludador> saludadores;
     @Autowired
     private ISaludador iSaludador;
+    int i=0;
 
     @PayloadRoot(localPart = "SaludarRequest", namespace="https://t4is.uv.mx/saludos")
 
     @ResponsePayload
     public SaludarResponse Saludar( @RequestPayload  SaludarRequest peticion) {
         SaludarResponse response= new SaludarResponse();
-        nombres[i]=peticion.getNombre();
+        nombres.add(peticion.getNombre());
         i++;
         response.setRespuesta("Hola "+peticion.getNombre());
 
-        Saludador saludador = new Saludador();
+        Saludador saludador =new Saludador();
         saludador.setNombre(peticion.getNombre());
         iSaludador.save(saludador);
+
         return response;
     }
 
@@ -39,15 +47,12 @@ public class EndPoint {
 
     @ResponsePayload
     public ModificarResponse Modificar( @RequestPayload  ModificarRequest peticion) {
-        ModificarResponse response= new ModificarResponse();
-        for (int x = 0; x < nombres.length; x++) {
-            if (peticion.getNombreOld().equals(nombres[x])){
-                nombres[x]=peticion.getNombreNew();
-                response.setRespuesta(peticion.getNombreOld()+" modificado por "+nombres[x]);
-            }else{
-                response.setRespuesta("Valor no encontrado");
-            }
-        }
+        ModificarResponse response = new ModificarResponse();
+        Optional<Saludador> x = iSaludador.findById(peticion.getId());
+        Saludador y=x.get();
+        y.setNombre(peticion.getNombreNew());
+        iSaludador.save(y);
+        response.setRespuesta("Modificado");
         return response;
     }
 
@@ -56,13 +61,32 @@ public class EndPoint {
     @ResponsePayload
     public PedirResponse Pedir() {
         PedirResponse response= new PedirResponse();
-        String todos="He saludado a: ";
-        for (int x = 0; x < nombres.length; x++) {
-          if(nombres[x]!=null){
-            todos=todos+ nombres[x] +", ";
-          }
+        String todos="";
+        saludadores=iSaludador.findAll();
+        for (Saludador x : saludadores) {
+            todos= todos +x.getId() + ": " + x.getNombre() +", ";
         }
         response.setRespuesta(todos);
+        return response;
+    }
+
+    @PayloadRoot(localPart = "BorrarRequest", namespace="https://t4is.uv.mx/saludos")
+
+    @ResponsePayload
+    public BorrarResponse Borrar( @RequestPayload  BorrarRequest peticion) {
+        BorrarResponse response= new BorrarResponse();
+        iSaludador.deleteById(peticion.getId());
+        return response;
+    }
+
+    @PayloadRoot(localPart = "PedirUnoRequest", namespace="https://t4is.uv.mx/saludos")
+
+    @ResponsePayload
+    public PedirUnoResponse PedirUno(@RequestPayload  PedirUnoRequest peticion) {
+        PedirUnoResponse response= new PedirUnoResponse();
+        Optional<Saludador> x = iSaludador.findById(peticion.getId());
+        Saludador y=x.get();
+        response.setRespuesta(y.getNombre());
         return response;
     }
 }
--- practica04/src/main/java/mx/uv/practica04/ISaludador.java
+++ practica04/src/main/java/mx/uv/practica04/ISaludador.java
@@ -2,6 +2,6 @@ package mx.uv.practica04;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface ISaludador extends CrudRepository<Saludador, Integer>{
+public interface ISaludador extends CrudRepository <Saludador, Integer>{
     
 }
